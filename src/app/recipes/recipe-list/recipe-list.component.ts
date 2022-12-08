2c502ff85a846bd8849f2cc5ada18ee031a94887
Mohammad Arif <arif_iq@yahoo.co.in>
2022-12-08 23:20:48 +0530

    Add header skeleton and recipe model

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
+/**
+ * Component decorator
+ *
+ * Decorator that marks a class as an Angular component and provides configuration
+ * metadata that determines how the component should be processed, instantiated,
+ * and used at runtime.
+ */
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
-export class RecipeListComponent {
 
+/**
+ * RecipeListComponent class
+ *
+ * This class is responsible for the recipe list component.
+ */
+export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'
+    ),
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'
+    ),
+  ];
 }
